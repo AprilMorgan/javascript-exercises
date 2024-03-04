cd10eb39325f4294a3c9919481636949ee4781a7
AprilMorgan <admorgan501@gmail.com>
2024-03-04 16:26:00 -0500

    Ex 1 and 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function ( word, times ) {
+  if (times < 0) return "ERROR";  //use a loop
+  let string = "";  //variable to hold string that will be returned
+  for (let i = 0; i < times; i++) { //create a loop that repeats a given number of times
+    string += word; //add given string to the result on each loop
+  }
+  return string;
 };
 
 // Do not edit below this line
