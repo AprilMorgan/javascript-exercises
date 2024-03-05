f389c418149071b4ca502b6ef675154f92d427ea
AprilMorgan <admorgan501@gmail.com>
2024-03-05 11:10:07 -0500

    Ex 6 and 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //fahrenheit to celsius, return 0
+  return Math.round ((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  //celsius to fahrenheit, return 32
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
